6da6e929c024d8dc546a4d8f97247465ca68bb00
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-07-17 12:58:32 +0300

    init

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,10 @@
+import styles from './index.module.scss'
+
 const Auth = () => {
     return (
-        <>
-        
-        </>
+        <div className={styles.wrap}>
+          <button className={styles.button}>Цвет</button>
+        </div>
     );
 }
 
