c35429c46609ebe60b61ab91955eca54f68ad14a
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-07-17 13:22:32 +0300

    init

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,14 +1,19 @@
 import styles from "./index.module.scss";
 
 const Auth = () => {
+  const style = {
+    div: {
+      'backgroundColor':'red'
+    }
+  }
   return (
-    <div className={styles.wrap}>
+    <div style={style.div} className={styles.wrap}>
       <button className={styles.button}>Цвет</button>
     </div>
   );
 };
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context:any) {
   const result = await fetch("");
   const data = await result.json();
 
