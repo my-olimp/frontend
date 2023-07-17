4893bfad86bf5fd6085fc3df50f52bc0930da307
Dmitriy <dimastepanov.1995@mail.ru>
2023-07-17 15:12:57 +0500

    context

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,11 +1,22 @@
-import styles from './index.module.scss'
+import styles from "./index.module.scss";
 
 const Auth = () => {
-    return (
-        <div className={styles.wrap}>
-          <button className={styles.button}>Цвет</button>
-        </div>
-    );
+  return (
+    <div className={styles.wrap}>
+      <button className={styles.button}>Цвет</button>
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const result = await fetch("");
+  const data = await result.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
 
 export default Auth;
