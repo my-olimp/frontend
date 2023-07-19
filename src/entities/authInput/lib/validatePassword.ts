8e5ac051b2a45a6bccbe9b05a64aed5a47c625c2
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-07-19 19:51:03 +0300

    some ui fixes, validatePassword.ts fixes

--- src/entities/authInput/lib/validatePassword.ts
+++ src/entities/authInput/lib/validatePassword.ts
@@ -7,7 +7,12 @@ export default function validatePassword(password: string) {
     if (password.match(/^[a-z]+$/) || password.match(/^\d+$/)) {
         return "Слабый пароль";
     }
-    if (password.match(/^[a-z\d]+$/) || password.match(/^(?=.*[a-z!@#$%^&*])[^A-Z\d]+$/) || password.match(/^[\d!@#$%^&*]+$/)) {
+    if (
+        password.match(/^[a-z\d]+$/) ||
+        password.match(/^(?=.*[a-z!@#$%^&*])[^A-Z\d]+$/) ||
+        password.match(/^[\d!@#$%^&*]+$/) ||
+        password.match(/^[a-zA-Z]+$/)
+    ) {
         return "Средний пароль";
     }
     if (password.match(/^(?=.*[a-z\d!@#$%^&*])|(?=.*[A-Z\d]).+$/)) {
