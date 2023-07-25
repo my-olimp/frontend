5b95352898385101d033c5afd159bae150dfb035
Dmitriy <dimastepanov.1995@mail.ru>
2023-07-25 21:04:08 +0500

    improved rouding AuthHelp/RegisterHelpComponent

--- src/features/authHelp/RegisterHelp/ui/ui.tsx
+++ src/features/authHelp/RegisterHelp/ui/ui.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
-import { LinkButton } from "@/entities/buttons/linkButton";
+import Link from "next/link";
+
 import styles from "./ui.module.scss";
 export const RegisterHelp: FC = ({}) => {
   return (
     <div className={styles.helpWrap}>
         <h2 className={styles.text}>Уже регистрировались в сервисах MyOlimp?</h2>
-        <a href="/signin" className={styles.link}>Войдите в учетную запись</a>
+        <Link href="/signin" className={styles.link}>Войдите в учетную запись</Link>
     </div>
   );
 };
