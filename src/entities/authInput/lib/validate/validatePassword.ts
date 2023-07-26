b8a8ca1909a12782e780d73732134bcd38979453
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-07-26 16:58:12 +0300

    some fixes, start confirm form developing

--- src/entities/authInput/lib/validate/validatePassword.ts
+++ src/entities/authInput/lib/validate/validatePassword.ts
@@ -3,22 +3,33 @@ export default function validatePassword(password: string) {
         return 'Пароль не может содержать в себе пробелы.';
     }
 
-    if (password.length < 5) {
+    if (password.length < 6) {
         return 'Слабый пароль';
     }
 
-    if (password.match(/^[a-z]+$/) || password.match(/^\d+$/)) {
+    if (
+        password.match(/^[a-z]+$/) ||
+        password.match(/^\d+$/) ||
+        (password.match(/123/) && password.match(/^\d+$/))
+    ) {
         return 'Слабый пароль';
     }
     if (
         password.match(/^[a-z\d]+$/) ||
         password.match(/^(?=.*[a-z!@#$%^&*])[^A-Z\d]+$/) ||
         password.match(/^[\d!@#$%^&*]+$/) ||
-        password.match(/^[a-zA-Z]+$/)
+        password.match(/^[a-zA-Z]+$/) ||
+        (password.match(/123/) &&
+            password.length > 6 &&
+            password.length < 8 &&
+            !password.match(/^\d+$/))
     ) {
         return 'Средний пароль';
     }
-    if (password.match(/^(?=.*[a-z\d!@#$%^&*])|(?=.*[A-Z\d]).+$/)) {
+    if (
+        password.match(/^(?=.*[a-z\d!@#$%^&*])|(?=.*[A-Z\d]).+$/) ||
+        (password.match(/123/) && password.length > 8)
+    ) {
         return 'Надежный пароль';
     }
 
--- src/widgets/AuthForm/AuthConfirmationForm/ui/ui.tsx
+++ src/widgets/AuthForm/AuthConfirmationForm/ui/ui.tsx
@@ -1,12 +1,101 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Logo from '@/entities/Logo/ui/ui';
+import styles from './ui.module.scss';
+import { Gapped } from '@/shared/Gapped';
+import { AuthButton } from '@/entities/buttons/authButton';
+import { LoginHelp } from '@/features/authHelp/LoginHelp';
 
 interface props {}
 
 export const ConfirmationForm: FC<props> = ({}) => {
+    const [first, setFirst] = useState('');
+    const [second, setSecond] = useState('');
+    const [third, setThird] = useState('');
+    const [fourth, setFourth] = useState('');
+    const mail: string = 'aaaaaa@gmail.com';
+    const number: string = '+71111111111';
+    const [isButtonDisabled, setButtonDisabled] = useState<'active' | 'disabled'>('disabled');
+
+    const [type, setType] = useState<'mail' | 'number'>('mail');
+
+    useEffect((): void => {
+        if (
+            first.length !== 0 &&
+            second.length !== 0 &&
+            third.length !== 0 &&
+            fourth.length !== 0
+        ) {
+            setButtonDisabled('active');
+        } else {
+            setButtonDisabled('disabled');
+        }
+    }, [first, second, third, fourth]);
+    const handleSubmit = (): void => {
+        console.log(first, second, third, fourth);
+    };
+
     return (
         <>
-            <Logo />
+            <Gapped className={styles.screen} vertical verticalAlign="middle">
+                <Gapped className={styles.center} gap="16px" vertical verticalAlign="middle">
+                    <Gapped
+                        gap="0px"
+                        vertical
+                        verticalAlign="middle"
+                        style={{ display: 'flex', width: '100%' }}>
+                        <Gapped className={styles.wrap} vertical gap="16px" verticalAlign="middle">
+                            <Gapped
+                                className={styles.headerWrap}
+                                gap="24px"
+                                verticalAlign="middle"
+                                vertical
+                                style={{ display: 'flex', width: '100%' }}>
+                                <Gapped
+                                    vertical
+                                    verticalAlign="middle"
+                                    alignItems="center"
+                                    gap="8px">
+                                    <Logo />
+                                    <h4 className={styles.text}>
+                                        Подтверждение{' '}
+                                        {type === 'mail' ? 'почты:' : 'номера телефона'}
+                                    </h4>
+                                    <h4 className={styles.subTitle}>
+                                        На {type === 'mail' ? 'почту' : 'номер'}{' '}
+                                        {type === 'mail' ? mail : number} был отправлен код, введите
+                                        его для завершения регистрации
+                                    </h4>
+                                </Gapped>
+                                <Gapped
+                                    vertical
+                                    verticalAlign="middle"
+                                    gap="24px"
+                                    style={{
+                                        marginBottom: '16px',
+                                        display: 'flex',
+                                        width: '100%',
+                                    }}>
+                                    <Gapped
+                                        className={styles.inputWrap}
+                                        vertical
+                                        verticalAlign="middle"
+                                        gap="24px"
+                                        style={{ display: 'flex', width: '100%' }}></Gapped>
+                                    <AuthButton
+                                        type="register"
+                                        width="fit-content"
+                                        height="medium"
+                                        use={isButtonDisabled}
+                                        onClick={handleSubmit}>
+                                        Подтвердить
+                                    </AuthButton>
+                                </Gapped>
+                            </Gapped>
+                        </Gapped>
+                    </Gapped>
+                    <LoginHelp />
+                </Gapped>
+            </Gapped>
         </>
     );
 };
