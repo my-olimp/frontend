bdc93c8dc2984aa8d121989b28c1d0d9538e6105
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-08-26 13:08:34 +0300

    some changes

--- src/features/authLoginHelp/ui/ui.tsx
+++ src/features/authLoginHelp/ui/ui.tsx
@@ -1,26 +1,20 @@
 import styles from './ui.module.scss';
-import { LinkButton } from '@/entities/buttons/linkButton';
-import { useRouter } from 'next/navigation';
 import { Checkbox } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import Link from '@mui/material/Link';
 
 export const AuthLoginHelp = () => {
     const [active, setActive] = useState<boolean>(false);
-    useEffect(() => {
-        console.log(active);
-    }, [active]);
-    const router = useRouter();
+
     return (
-        <>
-            <div className={styles.wrap}>
-                <div className={styles.checkWrap} onClick={() => setActive(!active)}>
-                    <Checkbox value={active} />
-                    <h1>Запомнить меня</h1>
-                </div>
-                <LinkButton link="/page" onClick={() => router.push('/page')}>
-                    Забыли пароль?
-                </LinkButton>
+        <div className={styles.wrap}>
+            <div className={styles.checkWrap}>
+                <Checkbox value={active} onClick={() => setActive(!active)} />
+                <h1>Запомнить меня</h1>
             </div>
-        </>
+            <Link href="/" underline="none" className={styles.link}>
+                Забыли пароль?
+            </Link>
+        </div>
     );
 };
