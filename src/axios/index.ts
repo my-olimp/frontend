c89722750785aead708d54bcd5cf34c1289a431c
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-09-01 21:58:37 +0300

    finally, fixed axios

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -4,7 +4,6 @@ const $api = axios.create({
     withCredentials: false,
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
-        'Un-Origin': location && location.origin,
         Accept: 'application/json, text/plain, */*',
         'Content-Type': 'application/json',
     },
