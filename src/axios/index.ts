dcd636de3bdeb67322913152088614d65087cd2e
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-09-03 16:51:38 +0300

    ui fix

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 const $api = axios.create({
     withCredentials: true,
     baseURL:
-        process.env.NODE_ENV === 'production' ? 'https://myob.xstl.ru/' : 'http://localhost:8000/',
+        process.env.NODE_ENV === 'production'
+            ? 'https://myo.xstl.ru/api/'
+            : 'http://localhost:8000/',
 });
 
 $api.interceptors.request.use((config) => {
