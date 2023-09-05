1998c59c2568e5eb8ee40b5d4ae4078cdf0b3b2a
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-09-05 23:09:59 +0300

    bug fix

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -9,6 +9,9 @@ const $api = axios.create({
             : 'http://localhost:8081/api/',
 });
 
+let isRefreshing = false;
+let refreshPromise = null;
+
 $api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
     return config;
@@ -20,15 +23,40 @@ $api.interceptors.response.use(
   },
   async (error) => {
     if (error.response && error.response.status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          const refreshTokenValue = localStorage.getItem('refreshToken');
+          if (!refreshTokenValue) {
+            // Handle the case where there's no refreshToken available
+            throw new Error('No refreshToken available');
+          }
 
-      try {
+          // Use a refreshPromise to prevent concurrent refresh attempts
+          refreshPromise = refreshToken();
+
+          // Wait for the refreshToken() to complete
+          await refreshPromise;
+
+          // Retry the original request with the new token
+          const originalRequest = error.config;
+          originalRequest.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+          return $api(originalRequest);
+        } catch (refreshError) {
+          // Handle the refreshToken error, e.g., logout the user
+          // You can also clear the tokens and redirect to login
+          throw refreshError;
+        } finally {
+          isRefreshing = false;
+          refreshPromise = null;
+        }
+      } else {
+        // If another request is already refreshing the token, wait for it to complete
+        await refreshPromise;
+        // Retry the original request with the new token
         const originalRequest = error.config;
-        await refreshToken()
-        const response = await $api(originalRequest)
-
-        return response;
-      } catch (error) {
-        throw error;
+        originalRequest.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+        return $api(originalRequest);
       }
     }
 
