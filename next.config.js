af8c4360910a00f989a3cffc6c165c9723e5d36c
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-09-14 21:51:11 +0300

    bug fix

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const withPWA = withPWAInit({
     dest: 'public',
     disable: isDev,
 
+    runtimeCaching: [
+        {
+            urlPattern: /\.(?:png|jpg|jpeg|gif|svg|webp)$/,
+            handler: 'NetworkOnly',
+        },
+    ],
+
     exclude: [
         ({ asset }) => {
             if (
@@ -22,23 +29,6 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Disable image caching
-        config.module.rules.forEach((rule) => {
-            if (rule.test && rule.test.toString().includes('png|jpe?g|gif|svg|webp')) {
-                rule.use = [
-                    {
-                        loader: rule.use[0].loader,
-                        options: {
-                            cache: false,
-                        },
-                    },
-                ];
-            }
-        });
-
-        return config;
-    },
     images: {
         formats: ['image/avif', 'image/webp'],
         domains: ['storage.yandexcloud.net'],
