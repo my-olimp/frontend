8084804818f623c8b8b221a3c4f8d96a5e6c3455
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-09-14 21:34:35 +0300

    bug fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 const withPWA = withPWAInit({
     dest: 'public',
     disable: isDev,
+
     exclude: [
         ({ asset }) => {
             if (
@@ -21,6 +22,23 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Disable image caching
+        config.module.rules.forEach((rule) => {
+            if (rule.test && rule.test.toString().includes('png|jpe?g|gif|svg|webp')) {
+                rule.use = [
+                    {
+                        loader: rule.use[0].loader,
+                        options: {
+                            cache: false,
+                        },
+                    },
+                ];
+            }
+        });
+
+        return config;
+    },
     images: {
         formats: ['image/avif', 'image/webp'],
         domains: ['storage.yandexcloud.net'],
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,28 +1,21 @@
 'use client';
-import { useAppSelector } from '@/hooks/useAppSelector';
 import { EditPersonalDataModal } from '@/widgets/EditPersonalDataModal';
 import { EditWorkDataModal } from '@/widgets/EditWorkDataModal';
 import { PersonalInfoBlock } from '@/widgets/PersonalInfoBlock';
 import { ProfileAvatar } from '@/widgets/ProfileAvatar';
 import { WorkBlock } from '@/widgets/WorkBlock';
 import { NextPage } from 'next';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from './index.module.scss';
 
 const Profile: NextPage = () => {
     const [editMode, setMode] = useState<'' | 'personal' | 'work'>('');
-    const { user } = useAppSelector((state) => state.auth);
 
-    const { push } = useRouter();
-
-    useEffect(() => {
-        if (editMode === 'personal' || editMode === 'work') {
-            document.body.style.overflow = 'hidden';
-        } else if (editMode === '') {
-            document.body.style.removeProperty('overflow');
-        }
-    }, [editMode]);
+    if (editMode === 'personal' || editMode === 'work') {
+        document.body.style.overflow = 'hidden';
+    } else if (editMode === '') {
+        document.body.style.removeProperty('overflow');
+    }
     return (
         <div className={styles.wrap}>
             <ProfileAvatar />
