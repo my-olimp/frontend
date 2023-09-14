775c32bcda9d142cbd6a909abe0e081d5a3f117b
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-09-14 21:37:16 +0300

    bug fix

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,17 +5,19 @@ import { PersonalInfoBlock } from '@/widgets/PersonalInfoBlock';
 import { ProfileAvatar } from '@/widgets/ProfileAvatar';
 import { WorkBlock } from '@/widgets/WorkBlock';
 import { NextPage } from 'next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 
 const Profile: NextPage = () => {
     const [editMode, setMode] = useState<'' | 'personal' | 'work'>('');
+    useEffect(() => {
+        if (editMode === 'personal' || editMode === 'work') {
+            document.body.style.overflow = 'hidden';
+        } else if (editMode === '') {
+            document.body.style.removeProperty('overflow');
+        }
+    }, [editMode]);
 
-    if (editMode === 'personal' || editMode === 'work') {
-        document.body.style.overflow = 'hidden';
-    } else if (editMode === '') {
-        document.body.style.removeProperty('overflow');
-    }
     return (
         <div className={styles.wrap}>
             <ProfileAvatar />
