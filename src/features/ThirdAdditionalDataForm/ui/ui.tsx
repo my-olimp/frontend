109aac9caab1a059e21e1401ea8850f06a03c909
Anton@habdevs <hramickih@yandex.ru>
2023-09-20 14:14:27 +0500

    fix:rollback commit 52e59f5 #5

--- src/features/ThirdAdditionalDataForm/ui/ui.tsx
+++ src/features/ThirdAdditionalDataForm/ui/ui.tsx
@@ -171,28 +171,6 @@ export const ThirdAdditionalDataForm: FC<props> = ({ progress, setProgress }) =>
                     </h2>
                     <h2 className={styles.descriptionDiscipline}>Популярные дисциплины</h2>
                 </div>
-                {/*<Stack spacing={2} sx={{ width: 300 }}>*/}
-                {/*    <Autocomplete*/}
-                {/*        freeSolo*/}
-                {/*        id="free-solo-2-demo"*/}
-                {/*        disableClearable*/}
-                {/*        options={disciplines.map((option) => option.name)}*/}
-                {/*        value={inputValue}*/}
-                {/*        onChange={(_, newValue) => setInputValue(newValue)}*/}
-                {/*        inputValue={inputValue}*/}
-                {/*        onInputChange={handleInputChange}*/}
-                {/*        renderInput={(params) => (*/}
-                {/*            <TextField*/}
-                {/*                {...params}*/}
-                {/*                label="Найти..."*/}
-                {/*                InputProps={{*/}
-                {/*                    ...params.InputProps,*/}
-                {/*                    type: 'search',*/}
-                {/*                }}*/}
-                {/*            />*/}
-                {/*        )}*/}
-                {/*    />*/}
-                {/*</Stack>*/}
                 <div className={styles.disciplesContainer}>
                     {disciplines?.map((discipline: Discipline) => (
                         <div
