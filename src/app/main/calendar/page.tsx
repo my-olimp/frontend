35bcf2f927b4e5719a7ee284433693feeb68d481
nics57 <ayanovnix@gmail.com>
2023-09-28 22:36:31 +0600

    nadoeli oshibki

--- src/app/main/calendar/page.tsx
+++ src/app/main/calendar/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-<<<<<<< HEAD
 import styles from './index.module.scss'
 import { useState, useEffect } from 'react';
 import { NextPage } from 'next';
@@ -233,16 +232,3 @@ const MyCalendar: NextPage = () => {
 };
 
 export default MyCalendar;
-=======
-import { NextPage } from 'next';
-
-const Calendar: NextPage = () => {
-    
-
-    return (
-        <></>
-    );
-};
-
-export default Calendar;
->>>>>>> 50864cfe0902d828f34e754098e74bac913d8b51
--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,14 +1,9 @@
 'use client';
-import { UnathorizedPopup } from '@/features/UnathorizedPopup';
 import { useAppSelector } from '@/hooks/useAppSelector';
 import { RefreshTokenComponent } from '@/shared/RefreshTokenComponent';
 import { Footer } from '@/widgets/Footer';
 import { Header } from '@/widgets/header';
-<<<<<<< HEAD
-import styles from '@/app/main/calendar/index.module.scss';
-=======
 import { useEffect, useState } from 'react';
->>>>>>> 50864cfe0902d828f34e754098e74bac913d8b51
 
 export default function Layout({ children }) {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -26,11 +21,6 @@ export default function Layout({ children }) {
         <>
             <Header />
             <RefreshTokenComponent />
-<<<<<<< HEAD
-=======
-            <main style={{ padding: '48px 0' }}>
-                {isOpen && <UnathorizedPopup isOpen={isOpen}/>}
->>>>>>> 50864cfe0902d828f34e754098e74bac913d8b51
                 {children}
             <Footer />
         </>
